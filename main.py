b83b2e9a327285094330a4492dcc671c45c014a6
Zen <true.grenight@gmail.com>
2020-12-15 10:41:25 +0200

    Auto crop detection

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ import subprocess
 Autoencoder
 """
 def check_executables():
-    """
-    Checking is all required executables reachable
-    """
+    """Checking is all required executables reachable"""
 
     if not find_executable('ffmpeg'):
         print('No ffmpeg')
@@ -25,10 +23,26 @@ def check_executables():
         sys.exit()
 
 
-def auto_crop():
-    """
-    Getting information about how source can be cropped
-    """
+def auto_crop(video):
+    """Getting information about how source can be cropped"""
+
+    script = f'ffmpeg -i {video.as_posix()} -vf fps=fps=5,cropdetect -f null -'.split()
+
+    r = subprocess.run(script, capture_output=True)
+    output = r.stderr.decode()
+
+
+    _, _, crop_x, crop_y = [ int(x) for x in re.findall(r"crop=([\d]+):([\d]+):([\d]+):([\d]+)", output)[-1]]
+    crop_left = crop_x//2
+    crop_right = crop_x//2
+    crop_top = crop_y//2
+    crop_bottom = crop_y//2
+    if crop_left + crop_right + crop_top + crop_bottom == 0:
+        print(':: No crop required')
+    else:
+        print(f":: Crop: {crop_left}:{crop_right}:{crop_top}:{crop_bottom}\nclip = core.std.Crop(clip, left={crop_left}, right={crop_right},top = {crop_top},bottom = {crop_bottom})\n")
+
+    return (crop_left, crop_right, crop_top, crop_bottom)
 
 
 def get_media_info(video):
@@ -36,12 +50,9 @@ def get_media_info(video):
     Getting media info from input video
     Width, Height, Frames, Fps
     """
-
-    # make script
     if not video.exists():
         print(f"Video {video.as_posix()} is not reachable")
         sys.exit()
-
 
     script = "import vapoursynth as vs\n" + \
     "core = vs.get_core()\n" + \
@@ -52,33 +63,29 @@ def get_media_info(video):
         fl.write(script)
 
     r = subprocess.run(f"vspipe get_media_info.py -i -".split(), capture_output=True)
-
+    output = r.stdout.decode()
 
     if len(r.stderr.decode()) > 0:
         print('Error in getting media info')
         print(r.stderr.decode())
         sys.exit()
 
-    output = r.stdout.decode()
-    # print(output)
-
     w = int(re.findall("Width: ([0-9]+)", output)[0])
     h = int(re.findall("Height: ([0-9]+)", output)[0])
     frames = int(re.findall("Frames: ([0-9]+)", output)[0])
     fps = int(re.findall("FPS: ([0-9]+)", output)[0])
     depth = int(re.findall("Bits: ([0-9]+)", output)[0])
+    print(f':: Media info:\n:: {w}:{h} frames:{frames}')
     return w, h, frames, fps, depth
+
 
 def argparsing():
     """
     Command line parsing and setting default variables
     """
-
     parser = argparse.ArgumentParser()
-
     io_group = parser.add_argument_group('Input and Output')
-    io_group.add_argument('--input', '-i', type=Path, help='Input File')
-
+    io_group.add_argument('--input', '-i', type=Path, required=True, help='Input File')
     parsed = vars(parser.parse_args())
     return parsed
 
@@ -88,4 +95,5 @@ if __name__ == "__main__":
     # Check initial requirements
     check_executables()
     args = argparsing()
-    print(get_media_info(args.get('input')))
+    w, h , frames, fps, depth = get_media_info(args.get('input'))
+    crops= auto_crop(args.get('input'))
